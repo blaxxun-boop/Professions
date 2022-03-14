d236b8df8193e502537e6bb9642b51d2e8c04432
blaxxun <blaxxun@blaxxun.blaxxun>
2022-03-14 17:31:17 +0100

    jotunn is fucking 💩

--- Professions/Professions.cs
+++ Professions/Professions.cs
@@ -15,7 +15,7 @@ namespace Professions;
 public class Professions : BaseUnityPlugin
 {
 	private const string ModName = "Professions";
-	private const string ModVersion = "1.0.0";
+	private const string ModVersion = "1.0.1";
 	private const string ModGUID = "org.bepinex.plugins.professions";
 
 	private static readonly ConfigSync configSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
@@ -190,7 +190,7 @@ public class Professions : BaseUnityPlugin
 	{
 		private static void Postfix(ref bool __result)
 		{
-			if (professionPanelInstance!.activeSelf)
+			if (professionPanelInstance?.activeSelf == true)
 			{
 				__result = true;
 			}
